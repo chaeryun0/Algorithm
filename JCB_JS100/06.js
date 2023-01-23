2f411d4c5404a76fe3b18dd55e3507d4367bc9cf
chaeryung <chaeryun00@gmail.com>
2023-01-23 16:24:38 +0900

    Solve: JCB 06_False

---
+++ JCB_JS100/06.js
@@ -0,0 +1,8 @@
+// 자바스크립트 문법에서 False로 취급하지 않는 것 (True로 취급하는 것) 하나를 고르세요.  
+// 1) NaN    
+// 2) 1  
+// 3)””    
+// 4) 0     
+// 5) undefined
+
+// JavaScript 에서는 null, undefined, 0, 빈 문자열, NaN, false 를 제외하고는 모두 참인 값으로 평가한다.
