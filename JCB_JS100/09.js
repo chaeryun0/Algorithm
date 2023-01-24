e459327fcec37f96761a8c84ee441b1bdb141774
chaeryung <chaeryun00@gmail.com>
2023-01-24 13:51:14 +0900

    Solve: JCB 09_ concat을활용한출력방법

---
+++ JCB_JS100/09.js
@@ -0,0 +1,16 @@
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = // 빈칸을 채워주세요.
+console.log(result);
+
+// 출력 2019/04/26 11:34:27
+
+var result = year.concat('/',month,'/',day,' ',hour,':',minute,':',second)
+
+// concatenate : ~을 연결하다, 잇다
+// concat() 변수를 하나로 이어서 출력할 수 있는 메서드
