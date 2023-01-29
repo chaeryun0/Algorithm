3209ac51bf204e5770042117f990f7f6b2305172
chaeryung <chaeryun00@gmail.com>
2023-01-29 12:29:12 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/19.js
+++ JCB_JS100/19.js
@@ -1,3 +1,4 @@
+// 방법 1 : 나눠서 계산
 const n = prompt("제곱을 구할 두 숫자를 입력해주세요.").split(" "); 
 const a = parseInt(n[0], 10); 
 const b = parseInt(n[1], 10); 
@@ -6,3 +7,7 @@ console.log(Math.pow(a, b));
 // Math.pow() 메서드는 제곱을 구함
 // n의 출력 결과가 문자형이기 때문에 숫자형으로 바꿔주고 계산해줘야함
 // pow() 메서드는 숫자형이 아니어도 숫자로 결과가 출력되지만 혹시 모를 오류에 대비해 숫자형으로 바꿔주고 계산함
+
+// 방법 2 : 한번에 계산
+const n = prompt("제곱을 구할 두 숫자를 입력해주세요.").split(" "); 
+console.log(Math.pow(parseInt(n[0], 10), parseInt(n[1], 10)));
