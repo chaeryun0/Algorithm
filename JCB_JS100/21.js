92f393aa2c5f99d33312ff04878a40e612926204
chaeryung <chaeryun00@gmail.com>
2023-01-30 01:00:40 +0900

    Solve: JCB 21_ set을만드는방법

---
+++ JCB_JS100/21.js
@@ -0,0 +1,7 @@
+// set을 만드는 방법으로 올바른 것 // 3, 5번
+
+// 1) var x = {1, 2, 3, 5, 6, 7}; 
+// 2) var x = {}; // 객체를 생성하는 리터럴 방식
+// 3) var x = new Set('javascript');
+// 4) var x = new Set(range(5)); // range는 알 수 없는 메서드
+// 5) var x = new Set();
