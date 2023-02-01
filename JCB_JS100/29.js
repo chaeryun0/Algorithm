3d19def255999a9e147de589c5cfa4c6c0749485
chaeryung <chaeryun00@gmail.com>
2023-02-01 12:20:28 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/29.js
+++ JCB_JS100/29.js
@@ -1,8 +1,18 @@
 // 알파벳이 대문자이면 YES를 아니면 NO를 출력
 
+// 풀이 1 - toUpperCase
 const n = prompt("알파벳을 입력하세요."); 
 if (n === n.toUpperCase()) {
  console.log("YES"); 
 } else { 
 	console.log("NO"); 
+}
+
+// 풀이 2 - includes()
+const Big = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const n = prompt("알파벳을 입력하세요.");
+if (Big.includes(n)) {
+  console.log("YES");
+} else {
+  console.log("NO");
 }
