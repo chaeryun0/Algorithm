b5d0285341ca6e3f5fafa7d8383d9c68734d9b4b
chaeryung <chaeryun00@gmail.com>
2023-02-03 00:23:10 +0900

    Solve: JCB 33_거꾸로출력하기

---
+++ JCB_JS100/33.js
@@ -0,0 +1,9 @@
+// 한 줄에 여러 개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력
+
+const n = prompt("5개의 숫자를 입력하세요.").split(" ").reverse(); 
+let result = ""; // 배열을 저장할 변수
+
+for (let i = 0; i < n.length; i++) { 
+	result += n[i]; 
+} 
+console.log(result);
