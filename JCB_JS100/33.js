1c0b4c9a178968e4be0ca002b3ab683ff9c6abc2
chaeryung <chaeryun00@gmail.com>
2023-02-03 00:23:55 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/33.js
+++ JCB_JS100/33.js
@@ -1,9 +1,16 @@
 // 한 줄에 여러 개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력
 
+// 풀이 1 - for문
 const n = prompt("5개의 숫자를 입력하세요.").split(" ").reverse(); 
 let result = ""; // 배열을 저장할 변수
 
 for (let i = 0; i < n.length; i++) { 
 	result += n[i]; 
 } 
+console.log(result);
+
+
+// 풀이 2 - 반복문 없이 값을 한번에 출력
+const n = prompt("5개의 숫자를 입력하세요");
+const result = n.split(" ").reverse().join(" ");
 console.log(result);
