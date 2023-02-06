0ec0b2e254bd2ce947bb15f166b2ee1f6f6a0ddf
chaeryung <chaeryun00@gmail.com>
2023-02-07 00:58:14 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/38.js
+++ JCB_JS100/38.js
@@ -1,6 +1,7 @@
 // 매번 1위부터 3위까지의 학생에게 상으로 사탕을 준다. 
 // 1위 ~ 3위 학생은 여러 명일 수 있고 1~3위 학생 중 중복되는 학생까지 포함하여 사탕을 사기로 한다. 학생들의 점수를 공백으로 구분하여 입력을 받고 사탕을 받을 학생의 수를 출력하세요.
 
+// 풀이 1
 const scores = prompt("학생들의 점수를 입력해주세요.")
   .split(" ")
   .map(n => parseInt(n, 10));
@@ -17,3 +18,18 @@ while (top.length < 4) {
 }
 
 console.log(count-1);
+
+
+// 풀이 2 
+const scores = "97 86 75 66 55 97 85 97 97 95".split(" ");
+scores.sort((a,b) => b - a);
+let top = scores.filter((e,i) => scores.indexOf(e) === i).slice(0,3);
+let count = 0;
+
+for(let i = 0; i < scores.length; i++){
+  let n = scores[i];
+  if(top.includes(n)) count ++;
+}
+
+console.log(top);    // ["97", "95", "86"]
+console.log(count); // 6
