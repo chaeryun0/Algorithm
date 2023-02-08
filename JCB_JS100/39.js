83e3ad19c6da6f2a5546cae8a50414e2da6f4391
chaeryung <chaeryun00@gmail.com>
2023-02-08 15:31:22 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/39.js
+++ JCB_JS100/39.js
@@ -8,3 +8,10 @@ console.log(word.split("q").join("e"));
 // 풀이 2 - replaceAll(정규표현식, 대체문자열)
 const word = prompt("문자를 입력하세요."); 
 console.log(word.replaceAll("q", "e"));
+
+
+// 풀이 3 - replaceAll(정규표현식, 대체문자열)
+const word = prompt("문자를 입력하세요."); 
+console.log(word.replaceAll(/q/g, "e"));
+
+// /q/g: 'q'를 '모두' 찾음
