f3a600f62bffc40e2381b64b43bd9d2a34b24af0
chaeryung <chaeryun00@gmail.com>
2023-02-08 15:30:35 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/39.js
+++ JCB_JS100/39.js
@@ -1,4 +1,10 @@
 // 문장이 입력되면 모든 q를 e로 바꾸는 프로그램
 
+// 풀이 1 - split, join
 const word = prompt("문자를 입력하세요."); 
 console.log(word.split("q").join("e"));
+
+
+// 풀이 2 - replaceAll(정규표현식, 대체문자열)
+const word = prompt("문자를 입력하세요."); 
+console.log(word.replaceAll("q", "e"));
