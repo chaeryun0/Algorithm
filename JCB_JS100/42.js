b88543d28919538e7e00c4cf686af7c79a15a8f6
chaeryung <chaeryun00@gmail.com>
2023-02-10 16:34:31 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/42.js
+++ JCB_JS100/42.js
@@ -4,6 +4,7 @@
 // 2020년 a월 b 인 실제로 있는 날입니다.
 // (13월 26일이나 2월 45일 같은 날짜는 주어지지 않습니다.)
 
+// 풀이 1 - Date객체에 ``을 이용하여 매개변수 a와 b를 넣어줌
 const month = prompt("월을 입력하세요.");
 const day = prompt("일을 입력하세요.");
 const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
@@ -13,3 +14,16 @@ function solution(a, b) {
 	console.log(week[theDay.getDay()]); 
 }
 solution(month, day);
+
+
+// 풀이 2 - solution함수의 매개변수로 month와 day를 넣어주고 Date객체에 년,월,일을 넣어줌 
+
+const month = prompt("월을 입력하세요.");
+const day = prompt("일을 입력하세요.");
+const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+function solution(a, b) {
+  const theDay = new Date(2020, a -1, b); // 월은 0부터 표현하므로 -1해줌
+  return week[theDay.getDay()];
+}
+console.log(solution(month, day));
