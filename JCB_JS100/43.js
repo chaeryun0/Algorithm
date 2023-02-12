54518527c5c782c9354c9e93b38e308d112927bc
chaeryung <chaeryun00@gmail.com>
2023-02-12 17:16:47 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/43.js
+++ JCB_JS100/43.js
@@ -1,4 +1,17 @@
 // 사용자에게 숫자를 입력 받고 이를 2진수로 바꾸고 그 값을 출력
 
+// 풀이 1 - toString()
 let n = Number(prompt("숫자를 입력해주세요."));
 console.log(n.toString(2));
+
+
+// 풀이 2 - while문
+
+let n = Number(prompt("숫자를 입력해주세요."));
+let arr = [];
+
+while(n) {
+  arr.push(n%2); 
+  n = Math.floor(n/2) 
+}
+console.log(arr.reverse().join(""));
