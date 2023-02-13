13429b7152ecc0de6db8f18703f65ed7d1bad35c
chaeryung <chaeryun00@gmail.com>
2023-02-13 12:56:50 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/44.js
+++ JCB_JS100/44.js
@@ -1,8 +1,24 @@
+// 풀이 1 - while문
+
 let n = prompt('숫자를 입력하세요.');
 let sum = 0;
 
 while(n !== 0){
   sum += (n % 10);
   n = Math.floor(n/10);
+}
+console.log(sum);
+
+
+// 풀이 2 - split()과 for문 활용, 배열을 분리하여 각 원소의 값을 모두 더하는 방식
+
+let n = prompt("숫자를 입력하세요")
+  .split("")
+	.map((n) => parseInt(n, 10));
+
+let sum = 0;
+
+for (let i = 0; i < n.length; i++) {
+  sum += n[i];
 }
 console.log(sum);
