2e7ad8f1d61f9a41ebb1c376f77c42724fb028f7
chaeryung <chaeryun00@gmail.com>
2023-02-15 15:46:06 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/46.js
+++ JCB_JS100/46.js
@@ -1,6 +1,6 @@
 // 1부터 20까지의(20을 포함) 모든 숫자를 일렬로 놓고 모든 자릿수의 총합
 
-// 1부터 20까지 원소를 하는 배열로 만들고 while문을 통해 10으로 나눈 나머지 값을 모두 더하는 방식
+// 풀이 1 - forEach(), while문
 let arr = [];
 let sum = 0; // 모든 자릿수를 더한 변수
 
@@ -13,4 +13,18 @@ arr.forEach((n)=>{
     n = Math.floor(n/10); // n을 10씩 나눠주면 순회할 때마다 10씩 나눠져서 종료됨
   }
 })
+console.log(sum);
+
+
+// 풀이 2 - for문, for in문
+let arr = [];
+let sum = 0;
+
+for(let i=0; i<20; i++){
+  arr[i] = i+1;
+}
+
+for(let i in arr){
+  sum += arr[i];
+}
 console.log(sum);
