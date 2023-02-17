9d36348fc11d6d9ec0d3f0a87350072956521be6
chaeryung <chaeryun00@gmail.com>
2023-02-18 00:22:53 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/49.js
+++ JCB_JS100/49.js
@@ -5,7 +5,11 @@ console.log(result.pop()); // 오름차순 정렬한 값에서 제일 마지막 
 
 
 // 풀이 2 - sort() 내림차순 정렬
-
 let result = prompt('10개의 숫자를 입력하세요').split(' ').map(n => parseInt(n, 10));
 result.sort((a, b) =>  b - a); // 내림차순 정렬
 console.log(result[0]);       // 내림차순 정렬한 값에서 맨 앞의 가장 큰 값을 출력함
+
+
+// 풀이 3 - Spread Operator(전개 연산자)
+const result = "10 9 8 7 6 5 4 3 2 1".split(' ');
+console.log(Math.max(...result));
