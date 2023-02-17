5a93de5bcb54f691b44f24756b7d089de5b95651
chaeryung <chaeryun00@gmail.com>
2023-02-18 00:22:24 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/49.js
+++ JCB_JS100/49.js
@@ -1,3 +1,11 @@
+// 풀이 1 - sort() 오름차순 정렬
 const result = prompt('10개의 숫자를 입력하세요').split(' ').map(n => parseInt(n, 10));
 result.sort((a, b) => a - b); // 오름차순 정렬
 console.log(result.pop()); // 오름차순 정렬한 값에서 제일 마지막 값을 빼냄 
+
+
+// 풀이 2 - sort() 내림차순 정렬
+
+let result = prompt('10개의 숫자를 입력하세요').split(' ').map(n => parseInt(n, 10));
+result.sort((a, b) =>  b - a); // 내림차순 정렬
+console.log(result[0]);       // 내림차순 정렬한 값에서 맨 앞의 가장 큰 값을 출력함
