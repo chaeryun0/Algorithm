82f0f7fffa8c428a276945d83d31cf4132fd6dd0
chaeryung <chaeryun00@gmail.com>
2023-02-21 12:33:58 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/54.js
+++ JCB_JS100/54.js
@@ -1,5 +1,6 @@
 // 스탬프에 적힌 숫자가 공백으로 구분되어 주어지면 이 숫자가 연속 수 인지 아닌지 "YES"와 "NO"로 판별하는 프로그램 작성
 
+// 풀이 1
 const num = prompt("스탬프의 숫자를 입력하세요.")
   .split(" ") 
   .sort();  
@@ -13,3 +14,20 @@ function solution(num){
 	return "YES"
 }
 console.log(solution(num));
+
+
+// 풀이 2
+
+const num = prompt("스탬프의 숫자를 입력하세요.")
+  .split(" ") // 문자열을 배열로 반환
+  .map((n) => parseInt(n, 10))
+  .sort((a, b) => a - b);
+
+let result = true;
+
+for (let i = 0; i < num.length - 1; i++) {
+  if (num[i] + 1 !== num[i + 1]) { 
+    result = false;
+  }
+}
+console.log(`${result ? "YES" : "NO"}`);
