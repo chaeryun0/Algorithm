d5a4107d20b6f41a09c56d87eebdfed016bcb714
chaeryung <chaeryun00@gmail.com>
2023-02-22 13:04:17 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/56.js
+++ JCB_JS100/56.js
@@ -1,5 +1,6 @@
 // 면적과 가장 비슷한 국가와 그 차이를 출력
 
+// 풀이 1
 const nationWidth = {
 	'korea': 220877,
 	'Rusia': 17098242,
@@ -25,3 +26,24 @@ for (let i in entry){
   }
 }
 console.log(item[0],item[1]-220877);
+
+// 풀이 1-1
+const w = nationWidth['korea'];
+delete nationWidth['korea']; 
+
+const entry = Object.entries(nationWidth); 
+const values = Object.values(nationWidth); 
+
+let gap = Math.max(...values); 
+let nation = [];
+
+for (let i in entry){
+  let min = Math.abs(entry[i][1] - w) 
+
+  if (gap > min){ 
+    gap = min
+    nation = entry[i];
+  }
+}
+// console.log(nation) // ["England", 242900]
+console.log(nation[0],nation[1]-220877); // England 22023 
