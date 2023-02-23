723734e61e2d4722f1e9d0e6450b81e700f95989
chaeryung <chaeryun00@gmail.com>
2023-02-23 12:16:08 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/57.js
+++ JCB_JS100/57.js
@@ -1,5 +1,6 @@
  // 0부터 1000까지 수에서 1은 몇 번이나 들어갔을까
 
+// 풀이 1 - for of문
 let arr = ''
 for(let i=0; i<=1000; i++){
 	arr += i;
@@ -8,11 +9,27 @@ for(let i=0; i<=1000; i++){
 count = 0;
 for(let j of arr){ 
 	if(j==1){ 
-		count++;
+	count++;
 	}
 }
 
 // for(let j of arr){
 //  j==1 ? count++ : 0
 
+console.log(count);
+
+
+// 풀이 2 - for in문
+
+let arr = ''
+for(let i=0; i<=1000; i++){
+	arr += i;
+}
+
+let count = 0
+for(let j in arr){ 
+	if(arr[j]==1){
+	count++;
+	}
+}
 console.log(count);
