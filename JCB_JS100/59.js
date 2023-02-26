6ccd48f29a01860bea494acc84b1672c1d182131
chaeryung <chaeryun00@gmail.com>
2023-02-26 19:35:31 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/59.js
+++ JCB_JS100/59.js
@@ -1,6 +1,12 @@
+// 풀이 1 - parseInt
 const str = prompt("문자를 입력하세요.");
 const n = 25+parseInt((str.length/2), 10);
 
 const left = str.padStart(n, '=');
 const right = left.padEnd(50,'=');
 console.log(right);
+
+// 풀이 2 - Math.floor
+const str = prompt("문자를 입력하세요.");
+const n = str.padStart(25 + Math.floor(str.length / 2), '=');
+console.log(n.padEnd(50, '='));
