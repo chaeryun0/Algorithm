0d2f4310d83ca6f154e4d2fb973c488270ab59cc
chaeryung <chaeryun00@gmail.com>
2023-02-27 11:28:18 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/60.js
+++ JCB_JS100/60.js
@@ -1,3 +1,8 @@
+// 풀이 1 - forEach
 const students = ['강은지','김유정','박현서','최성훈','홍유진','박지호','권윤일','김채리','한지호','김진이','김민호','강채연']
 students.sort()
         .forEach((n, index) => console.log(`번호: ${index + 1}, 이름: ${n}`))
+
+
+// 풀이 2 - map
+students.sort().map((n, index) => console.log(`번호: ${index + 1}, 이름: ${n}`));
