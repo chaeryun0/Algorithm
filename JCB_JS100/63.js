e722cdebc683aba839e0e62ff333055c2f18beea
chaeryung <chaeryun00@gmail.com>
2023-03-03 14:17:24 +0900

    Improve: 풀이 방법 추가

--- JCB_JS100/63.js
+++ JCB_JS100/63.js
@@ -15,3 +15,10 @@ for (let i = 0; i < arr.length; i++) {
   result += arr[i][0];
 }
 console.log(result);
+
+// 풀이 3 - forEach
+const arr = '복잡한 세상 편하게 살자'.split(' ');
+let result = '';
+
+arr.forEach(n => result += n[0])
+console.log(result);
