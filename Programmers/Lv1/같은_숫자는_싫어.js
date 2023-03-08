2e545056185be8eab185a3c91961e86f3f7b79dc
chaeryung <chaeryun00@gmail.com>
2023-03-08 11:41:01 +0900

    Improve: 풀이 방법 추가

--- Programmers/Lv1/같은_숫자는_싫어.js
+++ Programmers/Lv1/같은_숫자는_싫어.js
@@ -1,8 +1,15 @@
+// 풀이 1 - for문
 function solution(arr){
     var answer = [];
     for(var i=0; i<arr.length; i++){
         if(arr[i] !== arr[i+1]) 
-					answer.push(arr[i]);      
+	answer.push(arr[i]);      
     }
     return answer;
+}
+
+
+// 풀이 2 - filter()
+function solution(arr){
+    return arr.filter((val,index) => val != arr[index+1]);
 }
