3e9bdd6dade0dab42728c11d1730ad2c26344d66
chaeryung <chaeryun00@gmail.com>
2023-03-09 15:27:23 +0900

    Improve: 풀이 방법 추가

--- Programmers/Lv1/문자열_내_마음대로_정렬하기.js
+++ Programmers/Lv1/문자열_내_마음대로_정렬하기.js
@@ -1,3 +1,4 @@
+// 풀이 1
 function solution(strings, n) {
     strings.sort((a,b) => {
     if(a[n] === b[n]) // n번째 문자가 같을 경우 매개변수로 받은 a,b 문자열 전체를 비교
@@ -5,4 +6,14 @@ function solution(strings, n) {
         return (a[n] > b[n]) - (a[n] < b[n]); // 자릿수 비교
     })
     return strings;
+}
+
+
+// 풀이 2
+function solution(strings, n) {
+  return strings.sort((a, b) => {
+    if (a[n] > b[n]) return 1; // 인덱스 n번째 글자를 기준으로 오름차순 정렬
+    else if (a[n] < b[n]) return -1;
+    else return a > b ? 1 : -1; // 인덱스 n번째 글자가 같으면 사전순 정렬
+  });
 }
