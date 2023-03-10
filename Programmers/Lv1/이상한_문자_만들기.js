44f14036266e6526fccb02785a3356f277937827
chaeryung <chaeryun00@gmail.com>
2023-03-10 11:28:25 +0900

    Improve: 풀이 방법 추가

--- Programmers/Lv1/이상한_문자_만들기.js
+++ Programmers/Lv1/이상한_문자_만들기.js
@@ -17,3 +17,20 @@ function solution(s) {
     
     return answer.join(' ') 
 }
+
+
+function solution(s) {   
+    let answer = [];
+    let arr = s.split(' ') // ['try', 'hello', 'world']
+        
+    for(let i = 0; i<arr.length; i++){ 
+    let words = arr[i].split('')  // [[ 't', 'r', 'y' ],[..],[..]] 
+        .map((e,idx)=> 
+        idx % 2 === 0 ? e.toUpperCase() : e.toLowerCase()).join('')
+	// index를 기준으로 짝/홀 판별, 대소문자로 변환.join() 작은 배열 합치기
+
+        answer.push(words) // ['TrY', 'HeLlO', 'WoRlD']
+    }
+    
+    return answer.join(' ') // "TrY HeLlO WoRlD" 큰 배열 합치기
+}
