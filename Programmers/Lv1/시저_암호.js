aab1c191a7b94da8636bf2174e266e8b10c1969b
chaeryung <chaeryun00@gmail.com>
2023-03-11 12:07:30 +0900

    Improve: 풀이 방법 추가

--- Programmers/Lv1/시저_암호.js
+++ Programmers/Lv1/시저_암호.js
@@ -1,7 +1,29 @@
+// 풀이  1 - fromCharCode(), charCodeAt()
 function solution(s, n) {
   return s.split('').map(e => {
     if(e === ' ') return e;
       return e.toUpperCase().charCodeAt()+n > 90 ? 
 			String.fromCharCode(e.charCodeAt()+n -26) : String.fromCharCode(e.charCodeAt()+n)
 	    }).join('');
+}
+
+
+// 풀이 2 - for문, includes(), indexOf()
+function solution(s, n) {
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var answer= '';
+
+    for(var i =0; i <s.length; i++){
+        var text = s[i];
+        if(text == ' ') {
+            answer += ' '; 
+            continue;
+        }
+        var textArr = upper.includes(text) ? upper : lower;
+        var index = textArr.indexOf(text)+n;
+        if(index >= textArr.length) index -= textArr.length;
+        answer += textArr[index];
+    }
+    return answer;
 }
