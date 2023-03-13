88bcc27836a9bc815804c7561b6e20a85ce06457
chaeryung <chaeryun00@gmail.com>
2023-03-13 11:53:26 +0900

    Improve: 풀이 방법 추가

--- Programmers/Lv1/행렬의_덧셈.js
+++ Programmers/Lv1/행렬의_덧셈.js
@@ -12,3 +12,16 @@ function solution(arr1, arr2) {
 function sumMatrix(A,B){
   return A.map((a,i) => a.map((b,j) => A[i][j] + B[i][j]))
 }
+
+
+// 풀이 3 - for in문
+function sumMatrix(A, B) {
+    var answer = [];
+    for (let i in A) {
+        answer.push([])
+        for (let j in A[i]) {
+            answer[i].push(A[i][j] + B[i][j])
+        }
+    }
+    return answer;
+}
